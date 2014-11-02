2345600fa325ddc0549074314ca3057649203f39
btweinstein <btweinstein@gmail.com>
2014-11-02 18:24:42 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='popgen_feynman_pics',
+    version='.2',
+    packages=[''],
+    url='',
+    license='',
+    author='Bryan Weinstein',
+    author_email='bweinstein@seas.harvard.edu',
+    description=''
+)
